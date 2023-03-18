8f691380718f09de6ab7e9905a8a1678cbace4f8
osfoce <oselufortunatus2002@gmail.com>
2023-03-18 16:41:42 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+/**
+ * main - to print alphabet using putchar
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
