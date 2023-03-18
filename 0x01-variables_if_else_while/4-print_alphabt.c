85d59051774c488c1bd98be7b4e443eb9fa64b5e
osfoce <oselufortunatus2002@gmail.com>
2023-03-18 15:06:18 +0100

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,14 @@
 
 int main(void)
 {
-char alp[26] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
