d2eb5b97ed08f2fea2efdb42d0f3987796eb7be7
Osfoce <oselufortunatus2002@gmail.com>
2023-03-30 23:43:20 +0100

    alx 0x06-pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - array reverse
+ * a: array
+ * n: number of array element
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
