251a2662f897249c3bc4393cbab4147c56156770
Osfoce <oselufortunatus2002@gmail.com>
2023-04-20 00:38:47 +0100

    A Program that will Write a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: name to be printed
@@ -6,7 +7,7 @@
  *
  * Return: Return nothing
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
