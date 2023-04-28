86c9ae64827ae1133bbfcb38fc6111d28614e51c
Osfoce <oselufortunatus2002@gmail.com>
2023-04-28 23:53:24 +0100

    updated lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%u] %s\n", h->y, h->str);
+			printf("[%u] %s\n", h->x, h->str);
 		}
 		h = h->next;
 		y++;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,7 +11,7 @@
 typedef struct list_s
 {
         char *str;
-        unsigned int len;
+        unsigned int x;
         struct list_s *next;
 }list_t;
 
