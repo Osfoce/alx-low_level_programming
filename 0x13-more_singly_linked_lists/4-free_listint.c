b79144579c47e4cec44786a57104c1284e95740b
Osfoce <oselufortunatus2002@gmail.com>
2023-05-02 22:57:48 +0100

    a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: Head
+ *
+ * Return: returns 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *list;
+
+	if (head == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		list = head->next;
+		head = list;
+		free(head);
+
+	}
+}
+
