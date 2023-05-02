022dc07e7bad434e959aaa9bf2787cbe0259ce11
Osfoce <oselufortunatus2002@gmail.com>
2023-05-03 00:51:43 +0100

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,54 @@
 #include "lists.h"
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+/**
+ * looped_listint_count - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *slow, *flash;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	flash = (head->next)->next;
+
+	while (flash)
+	{
+		if (slow == flash)
+		{
+			slow = flash;
+			while (slow != flash)
+			{
+				nodes++;
+				slow = slow->next;
+				flash = flash->next;
+			}
+
+			slow = slow->next;
+			while (slow != flash)
+			{
+				nodes++;
+				slow = slow->next;
+			}
+
+			return (nodes);
+		}
+
+		slow = slow->next;
+		flash = (flash->next)->next;
+	}
+
+	return (0);
+}
 
 /**
  * free_listint_safe - frees a linked list
