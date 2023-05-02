4220f0149413f997464dbff03f035b513e45b57c
Osfoce <oselufortunatus2002@gmail.com>
2023-05-03 00:23:52 +0100

    Header_file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * struct listint_s - singly linked list.
+ * @n: integer.
+ * @next: points to the next node.
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure.
  * 
  */
 
