82f1ec28b6133ef9af4992b3d8b9e80c81bad9ab
Osfoce <oselufortunatus2002@gmail.com>
2023-06-11 14:32:21 +0100

    prints the multiple os args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiples of args passed
+ * @argc: the number of args passed
+ * @argv: an array os passed args
+ * Return: 0 if sucessful else 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, res = 0;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d", res);
+		return (0);
+	}
+	return (1);
+}
