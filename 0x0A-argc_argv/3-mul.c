37ee26c15534cade2795c1d39e09d1d4a73d244f
Osfoce <oselufortunatus2002@gmail.com>
2023-06-11 16:34:42 +0100

    prints the multiples of args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,17 @@ int main(int argc, char *argv[])
 {
 	int i, res = 1;
 
-	if (argc >= 2)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			res *= atoi(argv[i]);
 		}
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	printf("%d\n", res);
 	return (0);
