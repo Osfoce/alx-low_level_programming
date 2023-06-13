5ddb58245f7e5f7dc4e6a5f1226e737f56bad6c5
Osfoce <oselufortunatus2002@gmail.com>
2023-06-13 23:34:18 +0100

    fxn that creates an array of char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - allocate space for an array
+ * @c: Character
+ * @size: the integer
+ * Return: a pointer to an array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a:
+	char *i;
+	a = 0;
+
+	i = malloc(size * sizeof(char));
+
+	if (size == 0 && i == NULL)
+	{
+		return (NULL);
+	}
+
+	while (a < size)
+	{
+		i[a] = c;
+		a++;
+	}
+
+	i[a] = "\0";
+	return (i);
+}
