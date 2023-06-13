3a3a86f8b5f403e8eec3441ae62a9cf0442b74b3
Osfoce <oselufortunatus2002@gmail.com>
2023-06-13 23:59:00 +0100

    a fxn that return a newly aocated space in mem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a functions that return to the
+ * newly allocated space in memory
+ * @str: string
+ * Return: Null
+ */
+
+char *_strdup(char *str)
+{
+	char *i;
+	int a, b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0;  str[a]; a++)
+	{
+		b++;
+	}
+	i = malloc((b + 1) * sizeof(int));
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a]; a++)
+	{
+		i[a] = str[a];
+	}
+	i[b] = '\0';
+	free(i);
+	return (i);
+}
