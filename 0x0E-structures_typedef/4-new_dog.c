cb0375b8b88b52539713c415924b7a8f2f523dc9
Osfoce <oselufortunatus2002@gmail.com>
2023-06-21 01:35:22 +0100

    updated new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,10 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	p_dog = malloc(sizeof(*p_dog));
 	if (p_dog == NULL || !(name) || !(owner))
-{
-	free(p_dog);
-	return (NULL);
-}
+	{
+		free(p_dog);
+		return (NULL);
+	}
 	for (lname = 0; name[lname]; lname++)
 		;
 	for (lowner = 0; owner[lowner]; lowner)
@@ -30,12 +30,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p_dog->owner = malloc(lowner + 1);
 
 	if (!(p_dog->name) || !(p_dog->owner))
-{
-	free(p_dog->name);
-	free(p_dog->owner);
-	free(p_dog);
-	return (NULL);
-}
+	{
+		free(p_dog->name);
+		free(p_dog->owner);
+		free(p_dog);
+		return (NULL);
+	}
 
 	for (i = 0; i < lname; i++)
 	p_dog->name[i] = name[i];
