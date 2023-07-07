6e07dff69c2720040aadef1c62505feae5a2b3a6
osfoce <oselufortunatus2002@gmail.com>
2023-07-07 20:15:26 +0100

    a fxn that returns processor endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks if a machine is little or big
+ * Return: 0 if big 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int i, endia;
+
+	i = 1;
+	char *x = (char *) &i;
+
+	if (*x)
+		endia = (endia ? 1 : 0);
+	return (endia);
+}
